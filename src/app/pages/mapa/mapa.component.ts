e80b1d25575c7286852ed3e6a724bf8e1eb78e2a
hernanfloresramirez1987 <hernanfloresramirez1987@gmail.com>
2023-03-21 14:09:04 -0400

    subiendo mapas

--- src/app/pages/mapa/mapa.component.ts
+++ src/app/pages/mapa/mapa.component.ts
@@ -1,27 +1,37 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
+import { NavigationControl, Marker, Map, Popup, GeoJSONSource } from 'mapbox-gl';
 import { Feature, MapaService } from 'src/app/services/mapa.service';
 import { Geolocation } from '@capacitor/geolocation';
 import { environment } from 'src/environments/environment.prod';
+//import { DirectionsService } from '@mapbox/mapbox-sdk/services/directions';
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.component.html',
   styleUrls: ['./mapa.component.scss']
 })
-export class MapaComponent implements AfterViewInit {
-  mapa!: mapboxgl.Map;
-  marker!: mapboxgl.Marker;
+export class MapaComponent implements OnInit {
+  mapa!: Map;
+  //directions!: DirectionsService;
+  marker!: Marker;
   addresses: Feature[] = [];
   selectedAddress = null;
+  popupUbicacion = new Popup().setHTML('Estoy aqui')
+
+  nav = new NavigationControl();
+
   constructor(private mapboxServ: MapaService) {}
-  ngAfterViewInit(): void {
+
+  ngOnInit(): void {
     this.locate();
+    this.searchDirections();
   }
   async locate() {
     const coordinates = await Geolocation.getCurrentPosition();
     console.log('Current position:', coordinates);
-    this.drawMap(coordinates.coords.longitude, coordinates.coords.latitude);
+    //this.drawMap(coordinates.coords.longitude, coordinates.coords.latitude);
+    this.mapboxServ.buildMap();
   }
   search(event: any) {
     const searchTerm = event.target.value.toLowerCase();
@@ -36,37 +46,74 @@ export class MapaComponent implements AfterViewInit {
       this.addresses = [];
     }
   }
-  onSelect(address: Feature) {
-    //this.selectedAddress = address;
+  onSelect(address: Feature) { //this.selectedAddress = address;
     this.addresses = [];
     console.log('///////////', address.center)
-    this.drawMap(address.center[0], address.center[1])
-  }
-  drawMap(lng: number, lat: number) {
-    (mapboxgl as any).accessToken = environment.mapboxPK;
-    if(this.mapa === undefined) {
-      this.mapa = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v12',
-        center: [lng, lat],
-        //inf: 45,
-        zoom: 13.5
-      });
-      this.createMarker(lng, lat);
-    } else {
-      console.log('aun no existe un mapa')
-      this.mapa.setCenter([1, lat])
-    }
-    console.log(this.mapa)
-  }
-  createMarker(lng: number, lat: number) {
-    const marker = new mapboxgl.Marker({draggable: true})
-                    .setLngLat([lng, lat])
-                    .setRotation(45)
-                    .addTo(this.mapa);
-    marker.on('drag', () => {console.log('8****', marker.getLngLat())});
+    //this.drawMap(address.center[0], address.center[1])
+    this.mapboxServ.addMarker(address.center[0], address.center[1])
   }
   removeMarker() {
+
+  }
+  drawRoutes() {
+    console.log("Hola mundo")
+    this.mapa.addLayer({
+      id: 'ruta',
+      type: 'line',
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'LineString',
+            coordinates: [
+              [-65.7160422117201, -21.445767101120342],
+              [-65.71687850093241, -21.444416598540272],
+              [-65.71865894979871, -21.444416598540272],
+            ]
+          }
+        }
+      },
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      paint: {
+        'line-color': '#888',
+        'line-width': 8
+      }
+    });
+  }
+  searchDirections() {
+    //this.mapa.addControl(this.nav, 'top-right');
+    const startMarker = new Marker({ color: 'red' });
+    const endMarker = new Marker({ color: 'blue' });
+
+    // Agregar marcadores a las coordenadas de inicio y fin
+    startMarker.setLngLat([-65.7160422117201, -21.445767101120342]);
+    startMarker.addTo(this.mapa);
+
+    endMarker.setLngLat([-65.71865894979871, -21.444416598540272]);
+    endMarker.addTo(this.mapa);
+    //this.mapa.addControl(new DirectionsService, )
+
+  }
+
+  getDirections() {
+    const origin = [-74.5, 40]; // Ejemplo origen
+    const destination = [-73.9, 40.7]; // Ejemplo destino
+
+    this.mapa.addControl(this.nav, 'top-right');
+    const startMarker = new Marker({ color: 'red' });
+    const endMarker = new Marker({ color: 'blue' });
+
+    // Agregar marcadores a las coordenadas de inicio y fin
+    startMarker.setLngLat([-74.5, 40]);
+    startMarker.addTo(this.mapa);
+
+    endMarker.setLngLat([-74.5, 40.5]);
+    endMarker.addTo(this.mapa);
 
   }
 }
--- src/app/services/mapa.service.ts
+++ src/app/services/mapa.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 
+import * as mapboxgl from 'mapbox-gl';
+import { NavigationControl, Marker, Map, Popup, GeoJSONSource } from 'mapbox-gl';
+import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 export interface MapboxOutput {
   attribution?: string;
@@ -20,19 +23,72 @@ export interface Feature {
   providedIn: 'root'
 })
 export class MapaService {
+  readonly url = `https://api.mapbox.com/`;
 
-  readonly url = `https://api.mapboxPK/geocoding/v5/`;
-  constructor(private http: HttpClient) { }
+  mapbox =(mapboxgl as typeof mapboxgl)
+  mapa! : Map;
+  style = 'mapbox://styles/mapbox/streets-v12';
+  lat = -21.44;
+  lng = -65.71;
+  zoom = 11;
+
+  popupUbicacion = new Popup().setHTML('Estoy aqui')
+
+  constructor(private http: HttpClient) {
+    this.mapbox.accessToken = environment.mapboxPK
+  }
+
+  buildMap(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      try{
+        this.mapa = new mapboxgl.Map({
+          container: 'map',
+          style: this.style,
+          zoom: this.zoom,
+          pitch: 45,
+          center: [ this.lng, this.lat ]
+        });
+        this.mapa.addControl(new mapboxgl.NavigationControl)
+        this.addMarker(this.lng, this.lat);
+
+
+        const geocoder = new MapboxGeocoder({
+          accessToken: mapboxgl.accessToken,
+          mapboxgl: mapboxgl
+        });
+        this.mapa.addControl(geocoder);
+
+        resolve({
+          map: this.mapa
+        });
+      } catch(e) {
+        console.log(e);
+        reject(e);
+      }
+    })
+  }
+  addMarker(lng: number, lat: number) {
+    console.log(lng,lat)
+    const marker = new Marker({draggable: true})
+                    .setLngLat([lng, lat])
+                    .setRotation(45)
+                    .setPopup(this.popupUbicacion)
+                    .addTo(this.mapa);
+    this.mapa.setCenter([lng, lat]);
+  }
 
   searchBasic(parameters: string) {
-    const _url = `${this.url}mapbox.places/${parameters}.json?access_token=${environment.mapboxPK}`;
+    const _url = `${this.url}geocoding/v5/mapbox.places/${parameters}.json?access_token=${environment.mapboxPK}`;
+    console.log(_url);
     return this.http.get(_url)
       .pipe(map((res: MapboxOutput) => {
         return res.features;
       }));
   }
-  searchProximity(lng: number, lat: number, params: string) {
-    const _url = `${this.url}mapboxPK/${params}.json?proximity=${lng},${lat}&access_token=${environment.mapboxPK}`;
+
+  searchProximity(lngIni: number, latIni: number, lngFin: number, latFin: number, params: string) {
+    //const _url = `${this.url}mapboxPK/${params}.json?proximity=${lng},${lat}&access_token=${environment.mapboxPK}`;
+    const _url = `${this.url}directions/v5/mapbox/cycling/${lngIni},${latIni},${lngFin},${latFin}?steps=true&geometries=geojson&access_token=${environment.mapboxPK}`;
     console.log('URL: ', _url);
     return this.http.get(_url)
       .pipe(map((res: MapboxOutput) => {
